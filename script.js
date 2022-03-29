3483c3918d2ad27f8a39c5c17e39d9c1c2a6cd60
DevA-rora <dev.arora@stu.burgmann.act.edu.au>
2022-03-29 06:40:06 +0000

    Added more button to index.html, fixed script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-<button onclick="Function()">Text</button>
+<html>
+
+    <head> 
+        <titile> script.js </titile> 
+    </head>
+
+    <body>
+        <button onclick="Function()">Text</button>
+        <button onclick="Function2()">Text</button>
+    </body>
+</html>
